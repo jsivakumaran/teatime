aa5fba6b6b2daee0a67319a8ebd511dbd4549f94
jsivakumaran <jsivakumaran@outlook.com>
2016-01-10 15:25:54 -0800

    Add initial repository for full-stack angular tea time

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('teaTimeApp')
+  .directive('navbar', () => ({
+    templateUrl: 'components/navbar/navbar.html',
+    restrict: 'E',
+    controller: 'NavbarController',
+    controllerAs: 'nav'
+  }));
